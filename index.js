200c7990f1703c36557904495861d1792d50743e
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-25 22:59:04 +0530

    Implement OMDB API integration to fetch movies and their detailed info

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const apiKey = 63279049;
 
-fetch(`http://img.omdbapi.com/?apikey=${apiKey}`)
-.then(res => res.json())
-.then(data => console.log(data));
+const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=batman`)
+const data = await res.json();
+console.log(data.Search);
+
+for (const movie of data.Search) {
+      const movieDetails = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`).then(res => res.json());
+        console.log(movieDetails);
+}
